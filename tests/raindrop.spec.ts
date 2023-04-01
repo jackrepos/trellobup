000dc50fc2a0c749561a132c006448f79fb65f70
jack <akdodji@gmail.com>
2023-04-01 15:25:45 -0400

    2023-04-01

--- tests/raindrop.spec.ts
+++ tests/raindrop.spec.ts
@@ -4,22 +4,30 @@ import * as fs from 'fs';
 const config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
 
 test.beforeEach(async ({ page }) => {
-  // go to login page
+  // go to raindrop login page
   await page.goto(config.raindrop.url);
-  // make sure that there is an input name "email" and an input submit with the value "Sign in"
+
+  // Expect the page to contain an input name "email"
   await expect(page.locator('input[name="email"]')).toBeVisible();
+
+  // Expect the page to contain an input submit with the value "Sign in"
   await expect(page.locator('input[type="submit"][value="Sign in"]')).toBeVisible();
+
   // enter the email address into the "email" input field
   await page.fill('input[name="email"]', config.raindrop.username);
+
   // enter the password into the "password" input field
   await page.fill('input[name="password"]', config.raindrop.password);
+
   // click the "Sign in" button
   await page.click('input[type="submit"][value="Sign in"]');
+
   // wait for the page to load
-  // await page.waitForNavigation();
   await page.waitForURL(config.raindrop.url);
+
   // expect to be redirected to the Raindrop.io settings page config.raindrop.url
   await expect(page.url()).toBe(config.raindrop.url);
+
   // expect the page to contain a link that contains the text "Create new"
   await expect(page.locator('a:has-text("Create new")')).toBeVisible();
 
@@ -28,19 +36,11 @@ test.beforeEach(async ({ page }) => {
 test('Create backup', async ({ page }) => {
   // click the "Create new" link
   await page.click('a:has-text("Create new")');
-  // wait for an alert success message to appear
-  // await page.waitForSelector('div:has-text("Your new backup is being created")');
-  await page.getByText('Your new backup is being created').waitFor();
-  // expect the page to contain a main div with the text "Your new backup is being created"
-  // await expect(page.locator('div:has-text("Your new backup is being created")')).toBeVisible();
-  await expect(page.getByText('Your new backup is being created')).toBeVisible();
 
-  // await page.goto(config.raindrop.url);
-  // await page.click('text=Sign in');
-  // await page.fill('input[name="email"]', config.raindrop.username);
-  // await page.fill('input[name="password"]', config.raindrop.password);
-  // await page.click('input[type="submit"][value="Sign in"]');
-  // await page.waitForNavigation();
-  // await expect(page.locator('text=Settings')).toBeVisible();
+  // wait for an alert success message to appear
+  await page.getByText('Your new backup is being created').waitFor();
+
+  // expect the page to contain a main div with the text "Your new backup is being created"
+  await expect(page.getByText('Your new backup is being created')).toBeVisible();
 
 });
--- tests/trello.spec.ts
+++ tests/trello.spec.ts
@@ -11,21 +11,24 @@ test.beforeEach(async ({ page }) => {
   await page.click('button:has-text("Already have an account? Log in")');
 
   // Wait for the page to load the url
-  // await page.waitForURL('**trello\.com/login**');
-  // Expect the URL to contain "login"
   await page.locator('input[type="text"][name="user"]').waitFor();
+
+  // Expect the URL to contain "login"
   expect(page.url()).toContain('login');
 
   // Expect the page to contain an input field with the name "user"
   await expect(page.locator('input[type="text"][name="user"]')).toBeVisible();
+
+  // enter the username into the "user" input field
   await page.fill('input[type="text"][name="user"]', config.trello.username);
 
   // Click the "Log in" button
   await page.click('input[type="submit"][id="login"]');
 
-  // Expect to be redirected to the Atlassian login page
-  // await page.waitForURL('.*https?://(?:www\.)?atlassian\.com/login.*');
+  // Wait for the page to load the url
   await page.locator('input[name="password"]').waitFor();
+
+  // Expect to be redirected to the Atlassian login page
   expect(page.url()).toContain('atlassian.com/login');
 
   // Expect the page to contain an input field with the name "password"
@@ -37,17 +40,20 @@ test.beforeEach(async ({ page }) => {
   // Click the "Log in" button
   await page.click('button[id="login-submit"]');
 
-  // Expect to be redirected back to the Trello personal board page
+  // Wait for the page to load the url
   await page.waitForURL(config.trello.url);
+
+  // Expect to be redirected back to the Trello personal board page
   expect(page.url()).toBe(config.trello.url);
 });
 
 test('Go to Game Dev test', async ({ page }) => {
-  // Go to the Trello login page
+  // Go to the Trello "game dev" board
   await page.goto(config.trello.links.gamedev);
+
+  // Expect the page to contain a h1 with the text "Game Dev"
+  await expect(page.locator('h1')).toHaveText('Game Dev');
 
   // await expect(page.locator('.board-canvas')).toHaveText("Prendre une pause toutes les 2h");
   // await expect(page.locator('.list-card-title')).toHaveText("Prendre une pause toutes les 2h");
-  await expect(page.locator('h1')).toHaveText('Game Dev');
-
 });
